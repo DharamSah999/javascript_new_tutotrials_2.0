4a35b721dca5286763ef120a7941c971417307ec
DharamSah999 <bhagyanasah332@gmail.com>
2024-04-01 18:21:46 +0545

    Modified the variable file

--- 01_variable.js
+++ 01_variable.js
@@ -1,1 +1,9 @@
-const newJs = "This is about variable in javascript"
+// const newJs = "This is about variable in javascript"
+const accountId = 22024793
+let accountEmail = "bnsah@gmail.com"
+var accountpassword = "12345"
+accountcity = "Ludhiana"
+let name;
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountpassword, accountcity, name])
