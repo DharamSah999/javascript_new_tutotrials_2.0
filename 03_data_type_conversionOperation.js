f4df40766619aa07bb0f6ca63c34ae9ae0fe0bf3
DharamSah999 <bhagyanasah332@gmail.com>
2024-04-01 19:27:59 +0545

    Practices and Modified the Conversion Operatiion

---
+++ 03_data_type_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/* 
+"95" => 95
+"85sdf" => NaN
+true => 1 / false => 0
+*/
+
+
+let isLoggedIn = ""
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Bhagya" => true
+
+let anyNumer = 99
+let stringNumber = String(anyNumer)
+console.log(stringNumber);
+console.log(typeof stringNumber);
