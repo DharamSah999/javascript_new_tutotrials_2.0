8e6a481131f3b40eed0b331fdd214d7b51621ddf
DharamSah999 <bhagyanasah332@gmail.com>
2024-04-01 18:44:34 +0545

    Add data_type

---
+++ 02_data_types.js
@@ -0,0 +1,20 @@
+"use strict" // treat all JS code as newer version
+
+// Data types 
+/*
+    number => 2 to power 53
+    bigint
+    string = ""
+    boolean = true/false
+    null = standalone value
+    undefined 
+    symbol
+
+
+
+    object
+*/
+
+console.log(typeof "Rohan"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
