086bdc3b2a6fab5ae5e8939e04169f71f59ffaa0
DharamSah999 <bhagyanasah332@gmail.com>
2024-04-07 20:57:55 +0545

    Update string

---
+++ Primitive_Data_Type/01_String.js
@@ -0,0 +1,11 @@
+const name = "Rohan "
+const age = 10
+
+//( `` ) String interpulation
+
+console.log(`Name: ${name}, Age: ${age}, Brother of Dharmendra`);
+
+const gameName = new String("Ludo");
+
+console.log(gameName[2]);
+console.log(gameName.__proto__);
