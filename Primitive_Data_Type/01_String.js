1e192b2489a30396d54a8f513e47f0b49c8450c2
DharamSah999 <bhagyanasah332@gmail.com>
2024-04-08 21:21:38 +0545

    Update to Strings and String Methods

--- Primitive_Data_Type/01_String.js
+++ Primitive_Data_Type/01_String.js
@@ -1,11 +1,25 @@
-const name = "Rohan "
-const age = 10
+const name = "Rohan ";
+const age = 10;
 
 //( `` ) String interpulation
 
-console.log(`Name: ${name}, Age: ${age}, Brother of Dharmendra`);
+// console.log(`Name: ${name}, Age: ${age}, Brother of Dharmendra`);
 
 const gameName = new String("Ludo");
 
-console.log(gameName[2]);
-console.log(gameName.__proto__);
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+const contentString = "Hello, world";
+console.log(contentString.length);
+// console.log(contentString.charAt(2));
+// console.log(contentString.indexOf("o"));
+
+
+const newString = contentString.substring(0, 5); // Negative Value Ignore of substring methods
+console.log(newString);
+// console.log(contentString);
+
+
+const anotherStrings = contentString.slice(-12, -5);
+console.log(anotherStrings);
